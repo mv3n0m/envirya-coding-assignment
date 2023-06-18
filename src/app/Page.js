915be92c9820b3790f17fc75a61c221dd338c19b
Manish Roy <manish.11612939@lpu.in>
2023-06-19 02:15:36 +0530

    integrated LineGraph and DoughnutGraph to Page component

--- src/app/Page.js
+++ src/app/Page.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import LineGraph from './LineGraph'
+import DoughnutGraph from './DoughnutGraph'
+import { Loader } from 'components'
+
 
 function Page() {
+    const [ session, sessionSet ] = useState(sessionStorage.getItem("maxTime"))
+    const [ loading, setLoading ] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+    }, [])
+
     return (
-        <div></div>
+        <div className='page'>
+            { session && <LineGraph setLoading={ setLoading } /> }
+            <DoughnutGraph sessionSet={ sessionSet } setLoading={ setLoading } />
+            { loading && <Loader /> }
+        </div>
     )
 }
 
